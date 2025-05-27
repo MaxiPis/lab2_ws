b8cca838405d30ab493ef26995542809e166fe95
JOSE QUIMI <jquimi@uc.cl>
2025-05-27 15:48:58 -0400

    Creacion distance sensor

--- src/pkg_parte_2/nodes/distancia_sensor.py
+++ src/pkg_parte_2/nodes/distancia_sensor.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Vector3
+from cv_bridge import CvBridge
+import numpy as np
+
+#De momento asumire que el simulador usara el mismo topico de comunicacion que el del laboratorio 1#
+
 # Este nodo debe suscribirse al kinect y 
 # obtener la distancia a la pared izquierda y derecha
 
@@ -8,3 +19,30 @@
 # Se publica como (dl, dr)
 
 # Publicamos todo el tiempo
+
+
+class DistanceSensor(Node):
+    def __init__(self):
+        super().__init__('distance_sensor')
+        self.get_logger().info('Distance Sensor Node has been started')
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/image_raw',
+            self.listener_callback,
+            10)
+        self.publisher = self.create_publisher(Vector3, '/dl_dr', 10)
+        self.bridge = CvBridge()
+
+    def listener_callback(self, msg):
+        pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    distance_sensor = DistanceSensor()
+    rclpy.spin(distance_sensor)
+    distance_sensor.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
