f54283e7cb2e2ff25a1b5c013df7b72d7c2c35b4
JOSE QUIMI <jquimi@uc.cl>
2025-05-27 12:02:18 -0400

    Setpoint publisher

--- src/pkg_parte_2/nodes/publicador_setpoint.py
+++ src/pkg_parte_2/nodes/publicador_setpoint.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+import numpy as np
+
 # La idea de este nodo es que inicialice el setpoint y por tanto el controlador_pasillo
 # Envía un mensaje a setpoint que será el deseo, en este caso, es que sea 0
 # Me hace sentido que sólo el controlador funcione si es que hay un deseo, por tanto publica constantemente
+
+
+class SetpointPublisher(Node):
+    def __init__(self):
+        super().__init__('publicador_setpoint')
+        self.get_logger().info('Iniciando publicador de setpoint...')
+        # Inicializar el setpoint deseado
+        self.setpoint_wish = 0.0
+
+        # Crear publicador para el setpoint
+        self.setpoint_pub = self.create_publisher(Float64, 'setpoint', 1)
+        self.timer = self.create_timer(0.2, self.publish_setpoint)
+
+    def publish_setpoint(self):
+        msg = Float64()
+        msg.data = self.setpoint_wish
+        self.setpoint_pub.publish(msg)
+        self.get_logger().info(f'[SETPOINT] Publicando setpoint: {msg.data}')
